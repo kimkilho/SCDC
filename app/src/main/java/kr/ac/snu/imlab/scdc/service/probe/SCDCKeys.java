3068e95b7fd5132859ad30ee6fb6d1be5c651ddb
catharsist <catharsistk@gmail.com>
2015-07-28 18:48:27 +0900

    Generate an interface which contains the keys for SCDC

---
+++ app/src/main/java/kr/ac/snu/imlab/scdc/service/probe/SCDCKeys.java
@@ -0,0 +1,21 @@
+package kr.ac.snu.imlab.scdc.service.probe;
+
+/**
+ * Created by kilho on 15. 7. 28.
+ */
+public class SCDCKeys {
+
+  public static interface LabelKeys {
+    public static final String
+            ACTION_LABEL_LOG = "kr.ac.snu.imlab.scdc.Broadcasting.action" +
+            ".LABEL_LOG",
+            SLEEP_LABEL = "sleeping",
+            IN_CLASS_LABEL = "in class",
+            EATING_LABEL = "having a meal",
+            STUDYING_LABEL = "studying",
+            DRINKING_LABEL = "drinking";
+    // LABEL_TYPE = "label_type",
+    // IS_LABELLED = "is_labelled";
+  }
+
+}
